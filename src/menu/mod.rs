3a13b626ccde57ffd07adcee00fc8f514a5ff896
Graham Wihlidal <graham@wihlidal.ca>
2019-01-27 07:44:38 +0100

    Awesome main menu

--- src/menu/mod.rs
+++ src/menu/mod.rs
@@ -1,6 +1,8 @@
 #![allow(unused_imports)]
 
-use crate::{graphics, Context, KeyCode, MouseButton, MusicTrack, Settings, WorldData};
+use crate::{
+    graphics, Context, KeyCode, MainState, Point2, Color, MouseButton, MusicTrack, Settings, Vector2, WorldData,
+};
 
 pub struct MenuPhase {
     pub first_update: bool,
@@ -28,8 +30,48 @@ impl MenuPhase {
         }
     }
 
-    pub fn draw(&mut self, _settings: &Settings, _data: &mut WorldData, ctx: &mut Context) {
-        graphics::clear(ctx, [0.1, 0.7, 0.3, 1.0].into());
+    pub fn draw(&mut self, _settings: &Settings, data: &mut WorldData, ctx: &mut Context) {
+        let window_size = graphics::drawable_size(ctx);
+
+        graphics::clear(ctx, [0.0, 0.0, 0.0, 1.0].into());
+
+        let img_width = data.splash.width() as f32;
+        let img_height = data.splash.height() as f32;
+        let scale_x = window_size.0 as f32 / img_width;
+        let scale_y = window_size.1 as f32 / img_height;
+
+        graphics::draw(
+            ctx,
+            &data.splash,
+            graphics::DrawParam::new().scale(Vector2::new(scale_x, scale_y)),
+        )
+        .unwrap();
+
+        let text = graphics::Text::new(("Press Space To Begin", data.font, 96.0));
+
+        let text_width = text.width(ctx) as f32;
+        let text_height = text.height(ctx) as f32;
+
+        graphics::draw(
+            ctx,
+            &text,
+            graphics::DrawParam::new()
+                .dest(Point2::new(
+                    ((window_size.0 as f32 / 2.0) - (text_width / 2.0)) + 4.0,
+                    (window_size.1 as f32 - text_height - 20.0) + 4.0,
+                ))
+                .color(Color::from((0, 0, 0, 255))),
+        )
+        .unwrap();
+
+        graphics::draw(
+            ctx,
+            &text,
+            graphics::DrawParam::new()
+                .dest(Point2::new((window_size.0 as f32 / 2.0) - (text_width / 2.0), window_size.1 as f32 - text_height - 20.0))
+                .color(Color::from((255, 255, 255, 255))),
+        )
+        .unwrap();
     }
 
     pub fn handle_key(
