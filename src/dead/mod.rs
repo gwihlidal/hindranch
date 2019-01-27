feb6043b656cdba859256b8cd2a42a547a2355f6
Graham Wihlidal <graham@wihlidal.ca>
2019-01-27 07:43:48 +0100

    Awesome death screen

--- src/dead/mod.rs
+++ src/dead/mod.rs
@@ -1,11 +1,15 @@
 #![allow(unused_imports)]
 
-use crate::{graphics, Context, KeyCode, MouseButton, Settings, Sounds, WorldData};
+use crate::{
+    audio, graphics, Color, Context, DrawParam, KeyCode, MouseButton, Point2, Settings, Sounds,
+    WorldData,
+};
 
 pub struct DeadPhase {
     pub sounds: Sounds,
     pub first_update: bool,
     pub want_restart: bool,
+    pub wasted: audio::Source,
 }
 
 impl DeadPhase {
@@ -14,6 +18,7 @@ impl DeadPhase {
             sounds: Sounds::load(ctx),
             first_update: true,
             want_restart: false,
+            wasted: audio::Source::new(ctx, "/voice/wasted.ogg").unwrap(),
         }
     }
     pub fn update(&mut self, settings: &Settings, _data: &mut WorldData, _ctx: &mut Context) {
@@ -21,13 +26,33 @@ impl DeadPhase {
             println!("STATE: Dead");
             if settings.sounds {
                 self.sounds.play_death();
+                self.wasted.play().unwrap();
             }
             self.first_update = false;
         }
     }
 
-    pub fn draw(&mut self, _settings: &Settings, _data: &mut WorldData, ctx: &mut Context) {
-        graphics::clear(ctx, [1.0, 0.1, 0.1, 1.0].into());
+    pub fn draw(&mut self, _settings: &Settings, data: &mut WorldData, ctx: &mut Context) {
+        let window_size = graphics::drawable_size(ctx);
+
+        graphics::clear(ctx, [0.0, 0.0, 0.0, 1.0].into());
+
+        let text = graphics::Text::new(("WASTED", data.font, 256.0));
+
+        let text_width = text.width(ctx) as f32;
+        let text_height = text.height(ctx) as f32;
+
+        graphics::draw(
+            ctx,
+            &text,
+            DrawParam::new()
+                .dest(Point2::new(
+                    (window_size.0 as f32 / 2.0) - (text_width / 2.0),
+                    (window_size.1 as f32 / 2.0) - (text_height / 2.0),
+                ))
+                .color(Color::from((255, 0, 0, 255))),
+        )
+        .unwrap();
     }
 
     pub fn handle_key(
